907d39e87f30c70101813efad1d1a86f72435eb3
Correy Lim <correy_lim@sfu.ca>
2016-03-24 17:50:49 -0700

    Added GET operation get_read_token_op - Time to test it. Welp.

--- AuthServer.cpp
+++ AuthServer.cpp
@@ -204,16 +204,30 @@ void handle_get(http_request message) {
 					unordered_map<string,string>::const_iterator got = json_body.find(prop_it->first); // Looking for the property Password in the JSON Body
 					if( got->second == prop_it->second.str() ){ // The password passed in from the JSON Body matched the password in this entity in AuthTable
 						for (auto prop_it2 = properties.begin(); prop_it2 != properties.end(); ++prop_it2){
+							bool cond1 {false};
+							bool cond2 {false};
+							string partition;
+							string row;
 							if(prop_it2->first == "DataPartition"){
-								string partition {prop_it2->second.str()};
+								partition = prop_it2->second.str();
+								cond1 = true;
+							}
+							if(prop_it2->first == "DataRow"){
+								row = prop_it2->second.str();
+								cond2 = true;
+							}
+							if(cond1 == true && cond2 == true){
+								pair<status_code,string> result = do_get_token (data_table, partition, row, table_shared_access_policy::permissions::read);
+								if(result.first == status_codes::InternalError){
+									message.reply( status_codes::InternalError );
+									return;
+								}
+								else if(result.first == status_codes::OK){
+									message.reply( status_codes::OK, result.second );
+									return;
+								}
 							}
 						}
-						for (auto prop_it3 = properties.begin(); prop_it3 != properties.end(); ++prop_it3){
-							if(prop_it3->first == "DataRow"){
-								string row {prop_it3->second.str()};
-							}
-						}
-						// pair<status_code,string> result = do_get_token (data_table, partition, row, uint8_t permissions);
 					}
 				}
 			}
