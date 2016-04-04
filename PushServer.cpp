91a294fbdfa60f81dc6909435b863368c09b6ceb
Correy Lim <correy_lim@sfu.ca>
2016-04-04 09:00:33 -0700

    Adding UserServer.cpp and PushServer.cpp; necessary changes to CMakeLists.txt have been done accordingly; Pulled and adding ClientUtils.cpp and ClientUtils.h from Ted

---
+++ PushServer.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include <cpprest/http_listener.h>
+#include <cpprest/json.h>
+
+#include <was/common.h>
+#include <was/table.h>
+
+#include "TableCache.h"
+#include "make_unique.h"
+
+#include "azure_keys.h"
+#include "ClientUtils.h"
+
+using azure::storage::storage_exception;
+using azure::storage::cloud_table;
+using azure::storage::cloud_table_client;
+using azure::storage::edm_type;
+using azure::storage::entity_property;
+using azure::storage::table_entity;
+using azure::storage::table_operation;
+using azure::storage::table_request_options;
+using azure::storage::table_result;
+using azure::storage::table_shared_access_policy;
+// Added these two in
+using azure::storage::table_query;
+using azure::storage::table_query_iterator;
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::getline;
+using std::make_pair;
+using std::pair;
+using std::string;
+using std::unordered_map;
+using std::vector;
+
+using web::http::http_headers;
+using web::http::http_request;
+using web::http::methods;
+using web::http::status_code;
+using web::http::status_codes;
+using web::http::uri;
+
+using web::json::value;
+
+using web::http::experimental::listener::http_listener;
+
+using prop_vals_t = vector<pair<string,value>>;
+using prop_str_vals_t = vector<pair<string,string>>;
+
+constexpr const char* def_url = "http://localhost:34574";
+
+int main (int argc, char const * argv[]) {
+  cout << "PushServer: Parsing connection string" << endl;
+  table_cache.init (storage_connection_string);
+
+  cout << "PushServer: Opening listener" << endl;
+  http_listener listener {def_url};
+  listener.support(methods::GET, &handle_get);
+  //listener.support(methods::POST, &handle_post);
+  //listener.support(methods::PUT, &handle_put);
+  //listener.support(methods::DEL, &handle_delete);
+  listener.open().wait(); // Wait for listener to complete starting
+
+  cout << "Enter carriage return to stop PushServer." << endl;
+  string line;
+  getline(std::cin, line);
+
+  // Shut it down
+  listener.close().wait();
+  cout << "PushServer closed" << endl;
+}
---
+++ UserServer.cpp
@@ -0,0 +1,88 @@
+#include <exception>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+#include <cpprest/base_uri.h>
+#include <cpprest/http_listener.h>
+#include <cpprest/json.h>
+
+#include <pplx/pplxtasks.h>
+
+#include <was/common.h>
+#include <was/storage_account.h>
+#include <was/table.h>
+
+#include "TableCache.h"
+#include "make_unique.h"
+
+#include "azure_keys.h"
+#include "ServerUtils.h"
+#include "ClientUtils.h"
+
+using azure::storage::cloud_storage_account;
+using azure::storage::storage_credentials;
+using azure::storage::storage_exception;
+using azure::storage::cloud_table;
+using azure::storage::cloud_table_client;
+using azure::storage::edm_type;
+using azure::storage::entity_property;
+using azure::storage::table_entity;
+using azure::storage::table_operation;
+using azure::storage::table_query;
+using azure::storage::table_query_iterator;
+using azure::storage::table_result;
+
+using pplx::extensibility::critical_section_t;
+using pplx::extensibility::scoped_critical_section_t;
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::getline;
+using std::make_pair;
+using std::pair;
+using std::string;
+using std::unordered_map;
+using std::vector;
+
+using web::http::http_headers;
+using web::http::http_request;
+using web::http::methods;
+using web::http::status_code;
+using web::http::status_codes;
+using web::http::uri;
+
+using web::json::value;
+
+using web::http::experimental::listener::http_listener;
+
+using prop_vals_t = vector<pair<string,value>>;
+
+constexpr const char* def_url = "http://localhost:34572";
+
+int main (int argc, char const * argv[]) {
+
+  http_listener listener {def_url}; // Acknowledges the requests sent to the server; If the below did not exist, it would receive the requests but wouldn't do anything
+
+  cout << "Parsing connection string" << endl;
+  table_cache.init (storage_connection_string);
+
+  cout << "Opening listener" << endl;
+  listener.support(methods::GET, &handle_get);
+  listener.support(methods::POST, &handle_post);
+  listener.support(methods::PUT, &handle_put);
+  listener.support(methods::DEL, &handle_delete);
+  listener.open().wait(); // Wait for listener to complete starting
+
+  cout << "Enter carriage return to stop server." << endl;
+  string line;
+  getline(std::cin, line);
+
+  // Shut it down
+  listener.close().wait();
+  cout << "Closed" << endl;
+}
